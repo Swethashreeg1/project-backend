41e7a489c8a59f677bab61d760dde3ad989eb97e
SwethaShree.G <swethashree.g2022eee@sece.ac.in>
2024-08-14 12:06:15 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,7 +53,7 @@
 // };
 
 // module.exports = { createUser, userLogin };
-const User=require('../models/UserModel')
+const User=require('../models/userModel')
 const bcrypt = require("bcrypt")
 const jwt= require("jsonwebtoken")
 
